af7db493b97744d1699262a8dc9f6e09a558b82b
Opilane TTHK <opilane@tthk.ee>
2020-10-15 11:35:22 +0300

    IDCodeReader

---
+++ IDCodeReader/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace IDCodeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your ID code:");
+            string usersID = Console.ReadLine();
+
+            if (Validate(usersID))
+            {
+                Console.WriteLine("Welcome!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong format. Try again.");
+            }
+        }
+        public static bool Validate(string idCode)
+        {
+            if(idCode.Length == 11)
+            {
+                try
+                {
+                    long.Parse(idCode);
+                    return true;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"Wrong format: {e}");
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
